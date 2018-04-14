29a8e49fbc8a2ad7b5ed5f91363b13e40801e016
KhoaLe <khoa.le@uts.edu.au>
2018-04-14 10:50:42 +1000

    add code

---
+++ ex1/build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20180413-235231/???";
+const char *build_timestamp = "2018-04-13T23:52:31Z";
+const char *build_version = "1.0";
---
+++ example-uart-c/build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20180414-000027/???";
+const char *build_timestamp = "2018-04-14T00:00:27Z";
+const char *build_version = "1.0";
